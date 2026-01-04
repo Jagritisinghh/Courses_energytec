ba00c391a2a840f664e89e4e3b7386dea8609e1f
Jagritisinghh <singhjagriti583@example.com>
2026-01-04 22:28:51 +0530

    deployed backend and added the backend url

--- client/myapp/src/services/api.js
+++ client/myapp/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://courses-energytec-4.onrender.com/api';
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
 });
