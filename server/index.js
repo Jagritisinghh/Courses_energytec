32abf5b4d0ab58ca54c5f2f9173bebe3f5c4747c
Jagritisinghh <singhjagriti583@example.com>
2026-01-04 22:58:14 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app=express();
 
 app.use(cors());
 app.use(cors({
-  origin: 'https://courses-energytec-8.onrender.com/login',
+  origin: 'https://courses-energytec-8.onrender.com/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
